1e634fce4f49127e259ed96759dfcff5c09589f9
jeongmoobin <kongkoze@naver.com>
2026-09-15 16:10:20 +0900

    Add files via upload

---
+++ as2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int outcome;
+    char grades;
+
+    printf("결과 점수를 입력하세요(0-100): ");
+    scanf("%d", &outcome);
+    
+    switch(outcome/10)
+    {
+        case 10:
+        case 9:
+        grades = 'A';
+        break;
+
+        case 8:
+        grades = 'B';
+        break;
+
+        case 7:
+        grades = 'C';
+        break;
+
+        case 6:
+        grades = 'D';
+        break;
+
+        default:
+        grades = 'F';
+        break;
+        
+    }
+    printf("학점은%c", grades);
+    return 0;
+}
